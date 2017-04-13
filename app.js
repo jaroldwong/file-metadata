e41b6bf857cd28baeb8064891d17f21edeae8dab
Jarold Wong <jaroldw@gmail.com>
2017-04-13 13:07:33 -0700

    Setup multer upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var multer = require('multer');
+var upload = multer({ dest: 'upload/' });
+var app = express();
+
+app.use(express.static('views'));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.post('/', upload.single('file'), function (req, res, next) {
+    res.send({ size: req.file.size });
+})
+
+app.listen(3000, function() {
+  console.log('File Metadata Microservice Started');
+});
